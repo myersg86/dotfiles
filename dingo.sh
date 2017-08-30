12b291a2ee3cf64880fb0031c9e7bd1f8f5a14b4
Greg Myers <myersg86@gmail.com>
2017-08-30 13:07:32 -0700

    Update dingo.sh

--- dingo.sh
+++ dingo.sh
@@ -2,9 +2,18 @@
 set -exbE
 USERNAME="$1"
 
-if [ -n "$1"]; then
+# Check that username is provided
+if [ -z "$1"]; then
 	echo "No username supplied"
+	exit
 fi &&
+
+# Must be run with SUDO priveleges
+if [ "$EUID" -ne 0 ]
+  then echo "Please execute script with SUDO"
+  exit
+fi &&
+
 echo "preparing dotfiles for $1" &&
 
 cd "/home/$USERNAME/" &&
@@ -15,16 +24,16 @@ mv -vf "/home/$USERNAME/.vimrc" "/home/$USERNAME/dotfiles_old/" &&
 mv -vf "/home/$USERNAME/.vim_runtime/" "/home/$USERNAME/dotfiles_old/" &&
 mv -vf "/home/$USERNAME/.bashrc" "/home/$USERNAME/dotfiles_old/" &&
 mv -vf "/home/$USERNAME/.tmux.conf" "/home/$USERNAME/dotfiles_old/" &&
-# mv git-prompt.sh "/home/$USERNAME/dotfiles_old/" &&
-# mv git-completion.bash "/home/$USERNAME/dotfiles_old/" &&
+mv -vf "/home/$USERNAME/git-prompt.sh" "/home/$USERNAME/dotfiles_old/" &&
+mv -vf "/home/$USERNAME/git-completion.bash" "/home/$USERNAME/dotfiles_old/" &&
 /bin/rm -rvf "/home/$USERNAME/.vim";
 sudo -u "$USERNAME" mkdir -v "/home/$USERNAME/.vim" &&
 sudo -u "$USERNAME" git clone -v https://github.com/flazz/vim-colorschemes.git "/home/$USERNAME/.vim" &&
 sudo -u "$USERNAME" git clone -v git://github.com/amix/vimrc.git "/home/$USERNAME/.vim_runtime" &&
 sudo -u "$USERNAME" bash "/home/$USERNAME/.vim_runtime/install_basic_vimrc.sh" &&
 sudo -u "$USERNAME" mv -vf "/home/$USERNAME/.vimrc" "/home/$USERNAME/dotfiles_old/"
-# wget sudo -u "$USERNAME" https://raw.githubusercontent.com/myersg86/dotfiles/master/git-completion.bash &&
-# wget sudo -u "$USERNAME"  https://raw.githubusercontent.com/myersg86/dotfiles/master/git-prompt.sh &&
+sudo -u "$USERNAME" wget https://raw.githubusercontent.com/myersg86/dotfiles/master/git-completion.bash &&
+sudo -u "$USERNAME" wget  https://raw.githubusercontent.com/myersg86/dotfiles/master/git-prompt.sh &&
 sudo -u "$USERNAME" wget https://raw.githubusercontent.com/myersg86/dotfiles/master/.bashrc &&
 sudo -u "$USERNAME" wget https://raw.githubusercontent.com/myersg86/dotfiles/master/.vimrc &&
 sudo -u "$USERNAME" wget https://raw.githubusercontent.com/myersg86/dotfiles/master/.tmux.conf
