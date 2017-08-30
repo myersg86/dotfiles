738da7034785a8d5b80577b6401134a164b6a75f
Greg Myers <myersg86@gmail.com>
2017-08-30 12:58:12 -0700

    Update dingo.sh

--- dingo.sh
+++ dingo.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 set -exbE
-cd ~/ &&
-/bin/rm -rvf ~/dotfiles_old;
-mkdir ~/dotfiles_old &&
-mv -vf ~/.vim/ ~/dotfiles_old/ &&
-mv -vf ~/.vimrc ~/dotfiles_old/ &&
-mv -vf ~/.vim_runtime/ ~/dotfiles_old/ &&
-mv -vf ~/.bashrc ~/dotfiles_old/ &&
-mv -vf ~/.tmux.conf ~/dotfiles_old/ &&
-# mv git-prompt.sh dotfiles_old/. &&
-# mv git-completion.bash dotfiles_old/. &&
-/bin/rm -rvf ~/.vim;
-mkdir -v ~/.vim &&
-git clone -v https://github.com/flazz/vim-colorschemes.git ~/.vim &&
-git clone -v git://github.com/amix/vimrc.git ~/.vim_runtime &&
-bash ~/.vim_runtime/install_basic_vimrc.sh &&
-mv -vf ~/.vimrc ~/dotfiles_old/
-# wget https://raw.githubusercontent.com/myersg86/dotfiles/master/git-completion.bash &&
-# wget https://raw.githubusercontent.com/myersg86/dotfiles/master/git-prompt.sh &&
-wget https://raw.githubusercontent.com/myersg86/dotfiles/master/.bashrc &&
-wget https://raw.githubusercontent.com/myersg86/dotfiles/master/.vimrc &&
-wget https://raw.githubusercontent.com/myersg86/dotfiles/master/.tmux.conf
+USERNAME="$1"
+
+if [ -z "$1"]; then
+	echo "No username supplied"
+fi &&
+echo "preparing dotfiles for $1" &&
+
+cd "/home/$USERNAME/" &&
+/bin/rm -rvf "/home/$USERNAME/dotfiles_old" &&
+sudo -u "$USERNAME" mkdir "/home/$USERNAME/dotfiles_old" &&
+mv -vf "/home/$USERNAME/.vim/" "/home/$USERNAME/dotfiles_old/" &&
+mv -vf "/home/$USERNAME/.vimrc" "/home/$USERNAME/dotfiles_old/" &&
+mv -vf "/home/$USERNAME/.vim_runtime/" "/home/$USERNAME/dotfiles_old/" &&
+mv -vf "/home/$USERNAME/.bashrc" "/home/$USERNAME/dotfiles_old/" &&
+mv -vf "/home/$USERNAME/.tmux.conf" "/home/$USERNAME/dotfiles_old/" &&
+# mv git-prompt.sh "/home/$USERNAME/dotfiles_old/" &&
+# mv git-completion.bash "/home/$USERNAME/dotfiles_old/" &&
+/bin/rm -rvf "/home/$USERNAME/.vim";
+sudo -u "$USERNAME" mkdir -v "/home/$USERNAME/.vim" &&
+sudo -u "$USERNAME" git clone -v https://github.com/flazz/vim-colorschemes.git "/home/$USERNAME/.vim" &&
+sudo -u "$USERNAME" git clone -v git://github.com/amix/vimrc.git "/home/$USERNAME/.vim_runtime" &&
+sudo -u "$USERNAME" bash "/home/$USERNAME/.vim_runtime/install_basic_vimrc.sh" &&
+sudo -u "$USERNAME" mv -vf "/home/$USERNAME/.vimrc" "/home/$USERNAME/dotfiles_old/"
+# wget sudo -u "$USERNAME" https://raw.githubusercontent.com/myersg86/dotfiles/master/git-completion.bash &&
+# wget sudo -u "$USERNAME"  https://raw.githubusercontent.com/myersg86/dotfiles/master/git-prompt.sh &&
+sudo -u "$USERNAME" wget https://raw.githubusercontent.com/myersg86/dotfiles/master/.bashrc &&
+sudo -u "$USERNAME" wget https://raw.githubusercontent.com/myersg86/dotfiles/master/.vimrc &&
+sudo -u "$USERNAME" wget https://raw.githubusercontent.com/myersg86/dotfiles/master/.tmux.conf
